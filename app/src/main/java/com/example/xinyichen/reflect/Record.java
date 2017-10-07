6243c9cc3597c5a98de2c057d1edf9da6bde797b
Xin Yi Chen <xinyichen516@gmail.com>
2017-10-07 15:57:00 -0700

    made some edits to add record

---
+++ app/src/main/java/com/example/xinyichen/reflect/Record.java
@@ -0,0 +1,14 @@
+package com.example.xinyichen.reflect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Record extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record);
+
+    }
+}
